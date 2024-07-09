583955d1abbf2371a40b0d45fb836d4a7d76e1bb
khunzinshine <khunzinshine@gmail.com>
2024-07-09 19:26:57 +0630

    added hotjar

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -195,8 +195,8 @@ const CONFIG = {
   ],
   // Display articles from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
+    source: '', // medium | dev
+    username: '', // to hide blog section, keep it empty
     limit: 2, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
@@ -204,7 +204,7 @@ const CONFIG = {
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
-    id: '',
+    id: '5053138',
     snippetVersion: 6,
   },
   themeConfig: {
